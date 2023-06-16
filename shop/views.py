cc0d6e2b9a090eefce2da9d38bfc4e5955cca7bd
Sahil <kamblesahil976@gmail.com>
2023-06-16 17:37:00 +0530

    Category-Wise Product Display In E Commerce Website

--- shop/views.py
+++ shop/views.py
@@ -7,15 +7,17 @@ from math import ceil
 
 def index(request):
     products = Product.objects.all()
-    print(products)
-    n = len(products)
-    nSlides = n//4 + ceil((n/4)-(n//4))
-    # params = {'no_of_slides': nSlides,
-    #           'range': range(1, nSlides), 'product': products}
-    allProds = [[products, range(1, len(products)), nSlides], [
-        products, range(1, len(products)), nSlides]]
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item["category"] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
     params = {'allProds': allProds}
-    return render(request, 'shop/index.html', params)
+    return render(request, "shop/index.html", params)
 
 
 def about(request):
