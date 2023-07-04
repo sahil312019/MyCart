4b8ac803b4e54936c2605877960b95bd03a50640
Sahil <kamblesahil976@gmail.com>
2023-07-04 19:38:15 +0530

    Merging Blog App Into The Shop UI

--- shop/views.py
+++ shop/views.py
@@ -31,6 +31,7 @@ def about(request):
 
 
 def contact(request):
+    thank = False
     if request.method == "POST":
         name = request.POST.get('name', '')
         email = request.POST.get('email', '')
@@ -38,7 +39,8 @@ def contact(request):
         desc = request.POST.get('desc', '')
         contact = Contact(name=name, email=email, phone=phone, desc=desc)
         contact.save()
-    return render(request, 'shop/contact.html')
+        thank = True
+    return render(request, 'shop/contact.html', {'thank': thank})
 
 
 def tracker(request):
